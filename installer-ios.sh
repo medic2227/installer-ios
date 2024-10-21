caf65e8e86d4c3e4c8b9859ea59a9dfc7bc24652
Ahmed Medić <119355094+medic2227@users.noreply.github.com>
2024-10-21 20:42:52 +0200

    -

--- installer-ios.sh
+++ installer-ios.sh
@@ -12,10 +12,13 @@ elif [[ "$password" == "nitro" ]]; then
     wget https://raw.githubusercontent.com/medic2227/nitro/refs/heads/main/nitro.py
 elif [[ "$password" == "giftcard" ]]; then
     wget https://raw.githubusercontent.com/medic2227/giftcard/refs/heads/main/giftcard.py
+elif [[ "$password" == "unchecked nitro" ]]; then
+    wget https://raw.githubusercontent.com/medic2227/unchecked-nitro/refs/heads/main/unchecked-nitro.py
 elif [[ "$password" == "make me rich" ]]; then
     wget https://raw.githubusercontent.com/medic2227/giftcard/refs/heads/main/giftcard.py
     wget https://raw.githubusercontent.com/medic2227/nitro/refs/heads/main/nitro.py
     wget https://raw.githubusercontent.com/medic2227/card/refs/heads/main/card.py
+    wget https://raw.githubusercontent.com/medic2227/unchecked-nitro/refs/heads/main/unchecked-nitro.py
 else
     echo "Invalid password"
     exit 1
